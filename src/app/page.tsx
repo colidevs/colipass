e2edd018b554286005428475d85661d855a858b4
Astrid <astridnaivirt22@outlook.com>
2024-06-20 20:17:57 -0300

    Remover div padre y cambio de estilos en componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,20 @@ import {Card, CardContent, CardHeader} from "@/components/ui/card";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center">
-      <Card className="w-[400px]">
-        <CardHeader>
-          <Label className="place-self-center font-semibold" htmlFor="password">
-            Qv#:4dD2E&.sgCHW
-          </Label>
-        </CardHeader>
-        <CardContent className="flex justify-center gap-4">
-          <Button className=" bg-indigo-600 text-white" id="copy">
-            ◲ Copy
-          </Button>
-          <Button className="bg-sky-500 text-white" id="regenerate">
-            ↻ Regenerate
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
+    <Card className="m-auto w-96 scale-110 shadow-md">
+      <CardHeader>
+        <Label className="place-self-center text-lg font-semibold tracking-wide" htmlFor="password">
+          Qv#:4dD2E&.sgCHW
+        </Label>
+      </CardHeader>
+      <CardContent className="flex justify-center gap-4">
+        <Button className="rounded-full bg-indigo-600 text-white" id="copy">
+          ◲ Copy
+        </Button>
+        <Button className="rounded-full bg-sky-500 text-white" id="regenerate">
+          ↻ Regenerate
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
