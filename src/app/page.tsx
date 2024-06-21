013f4283f4750d84ec318dcfc4b2827af0d8340d
Astrid <astridnaivirt22@outlook.com>
2024-06-21 19:13:12 -0300

    Desacople de homepage más modificación y nuevos estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-import {Button} from "@/components/ui/button";
-import {Label} from "@/components/ui/label";
-import {Card, CardContent, CardHeader} from "@/components/ui/card";
+import CardPassword from "@/components/ui/CardPassword";
 
-export default function HomePage() {
-  return (
-    <Card className="m-auto w-96 scale-110 shadow-md">
-      <CardHeader>
-        <Label className="place-self-center text-lg font-semibold tracking-wide" htmlFor="password">
-          Qv#:4dD2E&.sgCHW
-        </Label>
-      </CardHeader>
-      <CardContent className="flex justify-center gap-4">
-        <Button className="rounded-full bg-indigo-600 text-white" id="copy">
-          ◲ Copy
-        </Button>
-        <Button className="rounded-full bg-sky-500 text-white" id="regenerate">
-          ↻ Regenerate
-        </Button>
-      </CardContent>
-    </Card>
-  );
+export default async function HomePage() {
+  return <CardPassword />;
 }
