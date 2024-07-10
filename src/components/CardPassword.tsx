30b8ffa2b257a792ea77f18f70906879b1e3498e
Astrid <astridnaivirt22@outlook.com>
2024-07-10 19:14:57 -0300

    Implementación de la función copiar al hacer click

--- src/components/CardPassword.tsx
+++ src/components/CardPassword.tsx
@@ -1,19 +1,27 @@
+"use client";
 import {Button} from "@/components/ui/button";
 import {Label} from "@/components/ui/label";
 import {Card, CardContent, CardHeader} from "@/components/ui/card";
 
 export default function CardPassword() {
+  const password = "Qv#:4dD2E&.sgCHW";
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+  };
+
   return (
     <Card className="m-auto size-36 w-96 shadow-md">
       <CardHeader>
         <Label className="text-center text-lg font-semibold tracking-wide" htmlFor="password">
-          Qv#:4dD2E&.sgCHW
+          {password}
         </Label>
       </CardHeader>
       <CardContent className="flex justify-center gap-4">
         <Button
           className="rounded-full border-indigo-700 bg-indigo-600 text-white shadow-md transition-transform ease-in-out hover:scale-110"
           id="copy"
+          onClick={handleCopy}
         >
           ◲ Copy
         </Button>
